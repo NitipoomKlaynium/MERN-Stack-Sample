43bbe5e86e902598c442d572d85dbcba10819f4e
NitipoomKlaynium <57699052+NitipoomKlaynium@users.noreply.github.com>
2023-06-08 08:22:38 +0700

    Practicing backend

---
+++ node_test/Part 1 Basic/people.js
@@ -0,0 +1,8 @@
+const people = ['Jack', 'Jame', 'John', 'Jane']
+const ages = [24, 27, 31, 19]
+
+console.log(people);
+
+module.exports = {
+  people, ages
+};
---
+++ node_test/Part 1 Basic/test.js
@@ -0,0 +1,10 @@
+// const name = 'Pure';
+
+// console.log(name);
+
+const greet = (name) => {
+    console.log(`hello, ${name}`)
+}
+
+greet('Ong');
+greet('Tong');
