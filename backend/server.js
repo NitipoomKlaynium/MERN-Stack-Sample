bcba7d1ca606ca1a058a5076003faa1068950c3d
NitipoomKlaynium <57699052+NitipoomKlaynium@users.noreply.github.com>
2023-06-29 09:41:03 +0700

    Init

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,9 @@
-require('dotenv').config()
-
 const express = require('express');
-
-// Express App
 const app = express();
 
-// Middleware
-app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
-})
-
-// Routes
 app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'});
+    console.log('Here');
+    res.send('Hi');
 });
 
-// Listen for request
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT);
-});
+app.listen(3000);
